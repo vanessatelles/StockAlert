6806eee2069c4946d36d65e6f01452fd25d723f4
Vanessa Telles <tellesvanessa07@gmail.com>
2022-01-27 17:15:24 -0300

    Add: SetCredentials method

--- EmailMessage.cs
+++ EmailMessage.cs
@@ -1,58 +1,41 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
 using System.Net.Mail;
 using System.Configuration;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-
 
 namespace StockAlert
 {
     public class EmailMessage
     {
-        string _server= ConfigurationManager.AppSettings["server"];
-        string _sender, _receiver, _username, _password, _message;
+        string _server, _sender, _receiver, _username, _password, _message;        
+
+        #region Properties
+        public String Server { get { return _server; } set { if(value!= null) _server = value; }}
+
+        public String Sender { get { return _sender; } set { if (value != null) _sender = value; }}
+
+        public String Receiver { get { return _receiver; } set { if (value != null) _receiver = value; }}
+
+        public String Username { get { return _username; } set { if (value != null) _username = value; }}
+
+        public String Password { get { return _password; } set { if (value != null) _password = value; }}
+
+        public String Message { get { return _message; } set { if (value != null) _message = value; }}
+        #endregion
+
         MailMessage mail = new MailMessage();
 
-        public String Server 
+        private void SetCredentials()
         {
-            get { return _server; }
-            set { _server = value; }
+            _server = ConfigurationManager.AppSettings["server"];
+            _sender = ConfigurationManager.AppSettings["sender"];
+            _receiver = ConfigurationManager.AppSettings["receiver"];
+            _username = ConfigurationManager.AppSettings["username"];
+            _password = ConfigurationManager.AppSettings["password"];
+            Console.WriteLine(_server);
         }
-
-        public String Sender 
-        {
-            get { return _sender; }
-            set { _sender = value; }
-        }
-
-        public String Receiver 
-        {
-            get { return _receiver; }
-            set { _receiver = value; }
-        }
-
-        public String Username 
-        {
-            get { return _username; }
-            set { _username = value; }
-        }
-
-        public String Password 
-        {
-            get { return _password; }
-            set { _password = value; }
-        }
-
-        public String Message 
-        {
-            get { return _message; }
-            set { _message = value; }
-        }
-
         public SmtpClient ServerConnection()
         {
+            SetCredentials();
             SmtpClient SmtpServer = new SmtpClient(_server);
             mail.From = new MailAddress(_sender);
             mail.To.Add(_receiver);
@@ -60,7 +43,6 @@ namespace StockAlert
             SmtpServer.Credentials = new System.Net.NetworkCredential(_username, _password);
             SmtpServer.EnableSsl = true;
 
-            Console.WriteLine("Conexão feita");
             return SmtpServer;
         }
 
--- StockAlert.cs
+++ StockAlert.cs
@@ -10,29 +10,16 @@ namespace StockAlert
         {
             string _chosenStock = args[0];
             float _salePrice = float.Parse(args[1], CultureInfo.InvariantCulture.NumberFormat);
-            float _purchasePrice = float.Parse(args[2], CultureInfo.InvariantCulture.NumberFormat);
-            
-            var _config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            float _purchasePrice = float.Parse(args[2], CultureInfo.InvariantCulture.NumberFormat);            
 
-            _config.AppSettings.Settings["username"].Value = args[3];
-            _config.AppSettings.Settings["receiver"].Value = args[4];
-            _config.AppSettings.Settings["password"].Value = args[5];
-            _config.Save(ConfigurationSaveMode.Modified);
-
-            EmailMessage msg = new EmailMessage();
-            msg.Username = ConfigurationManager.AppSettings["username"];
-            msg.Password = ConfigurationManager.AppSettings["password"];
-            msg.Sender = msg.Username + "@gmail.com";
-            msg.Receiver = ConfigurationManager.AppSettings["receiver"] + "@gmail.com";
-
+            EmailMessage msg = new EmailMessage();            
             msg.Message = ConfigurationManager.AppSettings["purchase"] + _chosenStock;
 
             Console.WriteLine(msg.Receiver);
 
             //msg.SendMessage();
 
-        }
-
+        }     
     }
 
 }
