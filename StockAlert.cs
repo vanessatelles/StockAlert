707562eb6f69897c5dcf39ccc784d9d631dc7eeb
Vanessa Telles <tellesvanessa07@gmail.com>
2022-02-09 21:27:42 -0300

    Add: Methods ArrayInfo and SetReference

--- StockAlert.cs
+++ StockAlert.cs
@@ -5,22 +5,22 @@ namespace StockAlert
     class Principal 
     {
         private static System.Timers.Timer _timer;
-        private static string? _choosenStock;
+        private static string? _chosenStock;
         private static float _salePrice;
         private static float _purchasePrice;
         
 
         static void Main(string[] args)
         {
-            //Reference values            
-            _choosenStock = args[0];
-            _salePrice = float.Parse(args[1], CultureInfo.InvariantCulture.NumberFormat);
-            _purchasePrice = float.Parse(args[2], CultureInfo.InvariantCulture.NumberFormat);
+            ArrayInfo(args);
+
+            //Reference values
+            SetReferenceValues(args[0], args[1], args[2]);            
                                     
             SetTimer();
 
             Console.WriteLine("Stock alert fires the event every two minutes. Press the Enter key to exit the application.\n");
-            Console.WriteLine($"Stock Alert is current using {_choosenStock} with reference values as: \nSale price: \t{_salePrice}\nPurchase price:\t{_purchasePrice}");
+            Console.WriteLine($"Stock Alert is current using {_chosenStock} with reference values as: \nSale price: \t{_salePrice}\nPurchase price:\t{_purchasePrice}");
             Console.ReadLine();
 
             _timer.Stop();
@@ -34,10 +34,28 @@ namespace StockAlert
         /// 
         /// </summary>
         
+        private static void ArrayInfo(string[] array)
+        {
+            if (array.Length != 3)
+            {
+                Console.WriteLine("Missing argument.");
+                Environment.Exit(0);
+            }
+        }
+
+
+        private static void SetReferenceValues( string chosenStock, string salePrice, string purchasePrice) 
+        {
+            _chosenStock = chosenStock;
+            _salePrice = float.Parse(salePrice, CultureInfo.InvariantCulture.NumberFormat);
+            _purchasePrice = float.Parse(purchasePrice, CultureInfo.InvariantCulture.NumberFormat);
+        }
+
+
         private static void StockValues()
         {
             StockData stockData = new StockData();
-            stockData.StockSymbol = _choosenStock;
+            stockData.StockSymbol = _chosenStock;
             stockData.PurchasePrice = _purchasePrice;
             stockData.SalePrice = _salePrice;
             stockData.CompareValues();
