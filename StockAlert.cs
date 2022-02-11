d7b403cc69f05e1f3ac3619117f55b0c2cb49b7c
Vanessa Telles <tellesvanessa07@gmail.com>
2022-02-11 19:15:30 -0300

    Add: StockAlert summaries.

--- StockAlert.cs
+++ StockAlert.cs
@@ -13,8 +13,7 @@ namespace StockAlert
         static void Main(string[] args)
         {
             ArrayInfo(args);
-            SetReferenceValues(args[0], args[1], args[2]);            
-                                    
+            SetReferenceValues(args[0], args[1], args[2]);          
             SetTimer();
 
             Console.WriteLine("Stock alert fires the event every two minutes. Press the Enter key to exit the application.\n");
@@ -24,12 +23,14 @@ namespace StockAlert
             _timer.Stop();
             _timer.Dispose();
 
-            Console.WriteLine("Terminating the application.");
-            
+            Console.WriteLine("Terminating the application.");            
         }
 
 
-       
+        /// <summary>
+        /// If the array length it's less than 3 it means one or more arguments are missing.
+        /// </summary>
+        /// <param name="array"></param>
         private static void ArrayInfo(string[] array)
         {
             if (array.Length != 3)
@@ -39,7 +40,12 @@ namespace StockAlert
             }
         }
 
-        
+        /// <summary>
+        /// Set the arguments passed by the user as reference values.
+        /// </summary>
+        /// <param name="chosenStock"></param>
+        /// <param name="salePrice"></param>
+        /// <param name="purchasePrice"></param>
         private static void SetReferenceValues( string chosenStock, string salePrice, string purchasePrice) 
         {
             _chosenStock = chosenStock;
@@ -48,7 +54,9 @@ namespace StockAlert
         }
         
 
-        
+        /// <summary>
+        /// It fires OnTimedEvent event every two minutes (120000 milliseconds).
+        /// </summary>
         private static void SetTimer()
         {
             _timer = new System.Timers.Timer(120000);
@@ -58,7 +66,12 @@ namespace StockAlert
         }
 
 
-        
+        /// <summary>
+        /// The method creates an instance of StockData, assigns it the reference values, 
+        /// and calls the CompareValues method.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
         public static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
             StockData stockData = new StockData();
