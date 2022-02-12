0316da29ad2eb2472ead2cb267ae61d1beb4bf8a
Vanessa Telles <tellesvanessa07@gmail.com>
2022-02-12 19:04:39 -0300

    Fix: EmailMessage details.

--- EmailMessage.cs
+++ EmailMessage.cs
@@ -6,13 +6,16 @@ namespace StockAlert
     public class EmailMessage
     {
         // Field
-        private string? _server, _port, _sender, _receiver, _username, _password, _message;        
-        
+        private string? _server, _port, _sender, _receiver, _username, _password, _message;
+
+
         // Properties
         public string Message { get { return _message; } set { if (value != null) _message = value; } }
-      
-        MailMessage mail = new MailMessage();
 
+    
+        /// <summary>
+        /// Initializes a new instance of the SmtpClient class by using configuration file settings.
+        /// </summary>
         public EmailMessage()
         {
             _server = ConfigurationManager.AppSettings["server"];
@@ -20,17 +23,20 @@ namespace StockAlert
             _sender = ConfigurationManager.AppSettings["sender"];
             _receiver = ConfigurationManager.AppSettings["receiver"];
             _username = ConfigurationManager.AppSettings["username"];
-            _password = ConfigurationManager.AppSettings["password"];            
+            _password = ConfigurationManager.AppSettings["password"];
         }
+       
+        
+        MailMessage mail = new MailMessage();
 
 
         /// <summary>
         /// Set the credentials, port, and enable SSL to the SMTP server.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>(SmtpClient) Returns an instance of the SmtpClient.</returns>
         private SmtpClient ServerConnection()
         {
-            SmtpClient smtpServer = new SmtpClient(_server); 
+            SmtpClient smtpServer = new SmtpClient(_server);
 
             mail.From = new MailAddress(_sender);
             mail.To.Add(_receiver);
@@ -43,7 +49,7 @@ namespace StockAlert
 
 
         /// <summary>
-        /// The method sends the message to the email address with the key "receiver" in the app.config file.
+        /// The method sends a message to the email address with the key "receiver" in the app.config file.
         /// </summary>
         public void SendMessage()
         {
@@ -63,4 +69,5 @@ namespace StockAlert
 
         }
     }
+
 }
